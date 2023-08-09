8c81c4cca62077a29cd7cbb5cf14dc3024b8d6f9
Peter Huynh <peterhyh@yahoo.com>
2023-08-09 11:23:01 -0500

    requesting money method added

--- oop_banking_app/app.py
+++ oop_banking_app/app.py
@@ -51,8 +51,25 @@ class BankUser(User):
             else:
                 print('Your PIN was incorrect!\nPlease try again...')
 
-    # def request_money(self):
-    #     request_amt = input('How much would you like to request? ')
+    def request_money(self, other_user):
+        requested_amt = float(
+            input('How much would you like to request from ' + other_user.name + '?'))
+        print('You are requesting $' + str(requested_amt),
+              'from', other_user.name.capitalize() + '? ')
+        print('User authentication is required...')
+
+        while True:
+            pin = input("Enter " + other_user.name.capitalize() + "'s PIN: ")
+            password = input('Enter your password: ')
+            if pin == other_user.pin and password == self.password:
+                other_user.balance -= requested_amt
+                self.balance += requested_amt
+                print('Request authorized')
+                print(other_user.name.capitalize(),
+                      'sent $' + str(requested_amt))
+                break
+            else:
+                print('Incorrect PIN and/or password.\nPlease try again...')
 
 
 bankUser = BankUser('Peter', '1234', 'pass123')
@@ -60,20 +77,19 @@ bankUser2 = BankUser('Trinh', '321', 'word123')
 
 
 bankUser.deposit()
-
 print(bankUser2.name.capitalize(),
       'has an account balance of: $' + str(bankUser2.balance))
 print(bankUser.name.capitalize(),
       'has an account balance of: $' + str(bankUser.balance))
 
+# bankUser.transfer_money(bankUser2)
+# print(bankUser2.name.capitalize(), 'balance is: $' + str(bankUser2.balance))
+# print(bankUser.name.capitalize(), 'balance is: $' + str(bankUser.balance))
 
-bankUser.transfer_money(bankUser2)
-print(bankUser2.name.capitalize(), 'balance is: $' + str(bankUser2.balance))
-print(bankUser.name.capitalize(), 'balance is: $' + str(bankUser.balance))
+
+bankUser2.request_money(bankUser)
 
 print(bankUser2.name.capitalize(),
       'has an account balance of: $' + str(bankUser2.balance))
 print(bankUser.name.capitalize(),
       'has an account balance of: $' + str(bankUser.balance))
-
-bankUser2.requesting_money(bankUser)
