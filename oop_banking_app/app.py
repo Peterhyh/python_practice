b143824aa5bbb11887d1177877d225f1fdebbf1e
Peter Huynh <peterhyh@yahoo.com>
2023-08-08 21:28:32 -0500

    oop banking app updat

--- oop_banking_app/app.py
+++ oop_banking_app/app.py
@@ -4,14 +4,44 @@ class User:
         self.pin = pin
         self.password = password
 
-    def change_name():
+    def change_name(self):
+        self.name = input('Enter a name: ')
 
-    def change_pin():
+    def change_pin(self):
+        self.pin = input('Enter a pin: ')
 
-    def change_password():
+    def change_password(self):
+        self.password = input('Enter a password: ')
 
 
-class BankUser:
+class BankUser(User):
     def __init__(self, name, pin, password):
         super().__init__(name, pin, password)
         self.balance = 0
+
+    def show_balance(self):
+        print('Your current balance is:', self.balance)
+
+    def deposit(self):
+        deposit_amount = input('How much would you like to deposit? ')
+        self.balance += float(deposit_amount)
+        print('You deposited $' + str(deposit_amount))
+        print('Your current balance is: $' + str(self.balance))
+
+    def withdraw(self):
+        withdraw_amount = input('How much would you like to withdraw? ')
+        self.balance -= float(withdraw_amount)
+        print('You withdrawed $' + str(withdraw_amount))
+        print('Your current balance is: $' + str(self.balance))
+
+
+'''
+    def transfer_money():
+
+    def request_money(): '''
+
+
+peter = BankUser('Peter', '1234', 'pass123')
+peter.show_balance()
+peter.deposit()
+peter.withdraw()
