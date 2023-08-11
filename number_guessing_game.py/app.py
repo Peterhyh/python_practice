ae452ccd86efc9d68b8b4871e5a4e416d4a80ec2
Peter Huynh <peterhyh@yahoo.com>
2023-08-10 20:58:02 -0500

    linear and binary search number guessing game complete

--- number_guessing_game.py/app.py
+++ number_guessing_game.py/app.py
@@ -28,40 +28,37 @@ def guess_random_num_binary(tries, start, stop):
         print('Program was unable to find the number!')
 
 
-guess_random_num_binary(10, 0, 200000)
-
-# def guess_random_num_linear(tries, start, stop):
-#     print('The number for the program to guess is:', answer)
-#     for guess in range(start, stop, 1):
-#         print("Number of tries left:", tries)
-#         if tries > 0:
-#             print('Program is guessing...', guess)
-#             if guess == answer:
-#                 print('Program guessed the correct number!')
-#                 exit()
-#             else:
-#                 tries -= 1
-#         else:
-#             print('Program was not able to guess the correct answer.')
-#             break
+def guess_random_num_linear(tries, start, stop):
+    print('The number for the program to guess is:', answer)
+    for guess in range(start, stop, 1):
+        print("Number of tries left:", tries)
+        if tries > 0:
+            print('Program is guessing...', guess)
+            if guess == answer:
+                print('Program guessed the correct number!')
+                exit()
+            else:
+                tries -= 1
+        else:
+            print('Program was not able to guess the correct answer.')
+            break
 
 
-# guess_random_num_linear(tries, 0, 10)
+while tries > 0:
+    print("Number of tries left:", tries)
+    guess = int(input("Guess a number between 0 and 10: "))
+    if guess == answer:
+        print('You guessed the correct number!')
+        quit()
+    elif guess > answer:
+        tries -= 1
+        print('Guess lower!')
+    elif guess < answer:
+        tries -= 1
+        print('Guess higher!')
+    else:
+        print('Please guess only the numbers between 0 and 10')
 
-
-# while tries > 0:
-#     print("Number of tries left:", tries)
-#     guess = int(input("Guess a number between 0 and 10: "))
-#     if guess == answer:
-#         print('You guessed the correct number!')
-#         quit()
-#     elif guess > answer:
-#         tries -= 1
-#         print('Guess lower!')
-#     elif guess < answer:
-#         tries -= 1
-#         print('Guess higher!')
-#     else:
-#         print('Please guess only the numbers between 0 and 10')
-
-# print('You have failed to guess the number: ', answer)
+print('You have failed to guess the number: ', answer)
+guess_random_num_linear(5, 0, 10)
+guess_random_num_binary(10, 0, 200)
