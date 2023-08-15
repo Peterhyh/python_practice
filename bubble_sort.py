b509fbacd386d2964939e55e429f347db063ca6d
Peter Huynh <peterhyh@yahoo.com>
2023-08-15 18:46:54 -0500

    bubble sort into a function

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,13 +6,18 @@ unsorted_list = [10, 23, 45, 14, 1, 5, 21]
 
 high_index = len(unsorted_list) - 1
 
-for i in range(high_index):
-    for j in range(high_index):
-        item = unsorted_list[j]
-        next = unsorted_list[j + 1]
 
-        if item > next:
-            unsorted_list[j] = next
-            unsorted_list[j + 1] = item
+def bubble_sort(list):
+    for i in range(high_index):
+        for j in range(high_index):
+            item = list[j]
+            next = list[j + 1]
 
-        print(unsorted_list, i, j)
+            if item > next:
+                list[j] = next
+                list[j + 1] = item
+
+            print(list, i, j)
+
+
+bubble_sort(unsorted_list)
