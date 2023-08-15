3f143da116aa6a0af50697245f6ccdb77d0084f4
Peter Huynh <peterhyh@yahoo.com>
2023-08-15 18:47:15 -0500

    bubble sort comment removal

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,5 @@
 unsorted_list = [10, 23, 45, 14, 1, 5, 21]
 
-# def bubble_sort(list):
-# declare high index
-# for in statement and pass in the high index value
 
 high_index = len(unsorted_list) - 1
 
