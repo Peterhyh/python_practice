3ef3064b0072cb3697cad8ab8ee57526613b1a2a
Peter Huynh <peterhyh@yahoo.com>
2023-08-16 18:51:34 -0500

    bubble sort testing update

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,20 +1,26 @@
-unsorted_list = [10, 23, 45, 14, 1, 5, 21]
+# unsorted_list = [10, 23, 45, 14, 1, 5, 21]
 
 
-high_index = len(unsorted_list) - 1
+# high_index = len(unsorted_list) - 1
 
 
-def bubble_sort(list):
-    for i in range(high_index):
-        for j in range(high_index):
-            item = list[j]
-            next = list[j + 1]
+# def bubble_sort(list):
+#     for i in range(high_index):
+#         for j in range(high_index):
+#             item = list[j]
+#             next = list[j + 1]
 
-            if item > next:
-                list[j] = next
-                list[j + 1] = item
+#             if item > next:
+#                 list[j] = next
+#                 list[j + 1] = item
 
-            print(list, i, j)
+#             print(list, i, j)
 
 
-bubble_sort(unsorted_list)
+# bubble_sort(unsorted_list)
+
+
+n = [1, 2, 3, 4]
+
+m = list(map(float, n))
+print(m)
