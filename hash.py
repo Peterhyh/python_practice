2a8b2f7f877b3630925a41fed6a1ddfc625ec59e
Peter Huynh <peterhyh@yahoo.com>
2023-09-20 16:48:46 -0500

    hash example file added

---
+++ hash.py
@@ -0,0 +1,11 @@
+hashed_string = hash("Hello, World!")
+
+# Hashing an integer
+hashed_integer = hash(42)
+
+# Hashing a tuple
+hashed_tuple = hash((1, 2))
+
+print(hashed_string)
+print(hashed_integer)
+print(hashed_tuple)
